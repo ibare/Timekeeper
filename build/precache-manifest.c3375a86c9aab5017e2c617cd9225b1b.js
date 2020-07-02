73d90ae16bbce1abc73b43e9c06d7588b50653cc
Kim mintae <ibare77@gmail.com>
2020-07-02 15:31:26 +0900

    add express static server

---
+++ build/precache-manifest.c3375a86c9aab5017e2c617cd9225b1b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "649f8c2e1f7ee2e3cdd08e7c75f1c910",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd5c1d869ccb15a82d30",
+    "url": "/static/js/2.b8235b6b.chunk.js"
+  },
+  {
+    "revision": "f8c2c5fbd98093610ec2",
+    "url": "/static/js/main.eb285e76.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
